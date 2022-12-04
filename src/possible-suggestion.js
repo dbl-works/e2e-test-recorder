f6ae617ebff90a9ed64b2882cd16ea8de071cc0c
Mohammed A <mohd@hey.com>
2022-12-05 01:27:12 +0300

    Future changes

---
+++ src/possible-suggestion.js
@@ -0,0 +1,11 @@
+let lastId = 0
+
+export class PossibleSuggestion {
+  constructor(name, description, args) {
+    // TODO: suggest ``cy.get('...').should('contain.text', '${selection.toString()}')`
+    this.id = (++lastId).toString()
+    this.name = name
+    this.description = description
+    this.args = args
+  }
+}
