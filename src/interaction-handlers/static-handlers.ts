dfa2f70a7ad3750e788aaaa7f1e5e7c0bd0c5963
Mohammed A <mohd@hey.com>
2022-12-08 23:15:37 +0300

    With example

--- src/interaction-handlers/static-handlers.ts
+++ src/interaction-handlers/static-handlers.ts
@@ -37,7 +37,7 @@ export class DocumentSelectionHandler {
 
     // In case of selection, we can't always rely on the target element for the selector.
     // What if it's a big paragraph coming from the DB? Thus, we can use the closest element that has a supported selector.
-    // If no parent element has a supported selector, we'll use the body.
+    // If no parent element has a supported selector, we'll use the body (this happens when selection is inside textarea).
     const selector = getSelector(
       selection?.focusNode?.parentElement?.closest(
         `${SUPPORTED_SELECTORS.map((s) => `[${s}]`).join(',')}, html`
